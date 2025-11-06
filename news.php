706fc77d0321fc54f070a5c6029bedc52dc0b144
DELL <krishnazarekar72@gmail.com>
2025-11-06 13:45:06 +0530

    change contact line

--- news.php
+++ news.php
@@ -248,8 +248,7 @@ $img_path = 'https://infspl.com/development/assets/front/img/';
 
 					<div class="cta-sameline">
 						<h2>Have any Question?</h2>
-						<p>We're here to help. Send us an email or call us at  +91 9921003190. Please feel free to contact
-							our expert.</p>
+						<p>We're here to help. Send us an email or call us at +91 9921003190.</p>
 						<a class="btn btn-alt" href="contact.html">Contact Us</a>
 					</div>
 
